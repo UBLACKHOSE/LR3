e3994de05f42df6e6e5607b8974dc1b35578bb78
UBLACKHOSE <35422221+UBLACKHOSE@users.noreply.github.com>
2022-11-29 14:58:31 +0700

    Start 3
    Mi pitalis

---
+++ LR3_3/Enemy.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LR3_3
+{
+    public class Enemy
+    {
+        private int hp, x, y;
+        private int damage { get; set; }
+
+        public Enemy(int _x = 0, int _y = 0, int _damage = 10, int _hp = 100)
+        {
+            x = _x;
+            y = _y;
+            damage = _damage;
+            hp = _hp;
+        }
+
+        //virtual public Location Moving(Location location)
+        //{
+        //    return location;
+        //}
+
+        //public void Hit()
+        //{
+
+        //}
+
+        public void Taking_damage(int _damage)
+        {
+            if (hp >= damage)
+            {
+                hp -= _damage;
+            }
+            else
+            {
+                throw new Exception("ТЫ СДОХ");/* Исправить */
+            }
+
+        }
+    }
+
+
+    class Moving_Enemy : Enemy
+    {
+        Moving_Enemy(int _x = 0, int _y = 0, int _damage = 10, int _hp = 100) : base(_x,_y,_damage,_hp) { }     
+    }
+
+}
